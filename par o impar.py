983cd341c4b1978b72f201e2a74c7b093f444ea9
TheRappas <maicol.ricom@uniagustiniana.edu.co>
2025-04-02 21:45:36 -0500

    Add files via upload

---
+++ par o impar.py
@@ -0,0 +1,18 @@
+#Par o Impar
+def par_ou_impar(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
+
+numero = int(input("Digite um número: "))
+if (numero % 2== 0):
+  print("EVEN POSITIVE")
+elif (numero %-2==-1 and -0):
+  print("EVEN NEGATIVE")
+elif (numero % 2 == 0):
+  print("ODD POSITIVE")
+elif (numero ==0):
+  print("Null")
+else:
+  print("ODD NEGATIVE")
